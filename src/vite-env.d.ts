972d98b762d4217700c6b962d1f97cc6ed466507
Clopezgg <chlopezg9@gmail.com>
2026-07-11 15:03:01 -0600

    Declarar metadatos de versión de la aplicación

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare const __APP_VERSION__: string;
+declare const __APP_COMMIT_SHA__: string;
+declare const __APP_BUILD_DATE__: string;
+declare const __APP_RELEASE_URL__: string;
