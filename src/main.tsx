6f8202ae248a4d73a91d81a62cca5d5e2447521c
Clopezgg <chlopezg9@gmail.com>
2026-07-11 18:31:29 -0600

    Cargar estilos de ficha digital

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './v2.css';
 import './v3.css';
+import './v3-card.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
 
